db65d64ef7bfadefc654787b31704a9cf8e63811
Yannick Dubreuil <yannick.dub.7@gmail.com>
2022-03-16 14:26:42 +0100

    All JUnit tests corrects

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,12 @@ package java8.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Date;
 
 import org.junit.Test;
@@ -18,13 +22,17 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+		Date date = new Date(117, 01, 12);
+		// System.out.println(date);
 
 		// TODO transformer la date en Instant
+		Instant instant = date.toInstant().plus(1, ChronoUnit.DAYS);
+		// System.out.println(instant);
 
 		// TODO transformer la date en LocalDate
 
-		LocalDate result = null;
+		LocalDate result = instant.atZone(ZoneOffset.UTC).toLocalDate();
+		// System.out.println(result);
 
 		// TODO valoriser les différentes variables afin de rendre le test passant
 
